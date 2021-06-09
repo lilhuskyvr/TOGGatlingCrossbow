d54c088010e83eda8005da129067870551de018e
Lil Husky VR <lilhuskyvr@gmail.com>
2021-06-10 00:02:23 +1000

    version 1.0

---
+++ TOGGatlingCrossbow/TOGGatlingCrossbowPatch.cs
@@ -0,0 +1,26 @@
+﻿using TOGModFramework;
+using UnityEngine;
+
+namespace TOGGatlingCrossbow
+{
+    public class TOGGatlingCrossbowPatch : MonoBehaviour
+    {
+        public void Inject()
+        {
+            EventManager.OnCrossbowBoltFired += EventManagerOnonCrossbowBoltFired;
+        }
+
+        private void EventManagerOnonCrossbowBoltFired(CrossBowScript crossBowScript)
+        {
+            if (!crossBowScript.isLoaded)
+            {
+                crossBowScript.isLoaded = true;
+                crossBowScript.isStringPulled = true;
+                crossBowScript.Bolt.GetComponent<MeshRenderer>().enabled = true;
+                crossBowScript.EquipControl.SetCrossBowLoaded(true);
+                crossBowScript.BowString.localPosition =
+                    new Vector3(0.0f, crossBowScript.BowString.localPosition.y, 0.008f);
+            }
+        }
+    }
+}
