e1e2415223eae3b8c63c0474f89d038341a273ad
Lil Husky VR <lilhuskyvr@gmail.com>
2021-06-10 21:33:59 +1000

    Completed 1.1 with semi auto mode

--- TOGGatlingCrossbow/TOGGatlingCrossbowPatch.cs
+++ TOGGatlingCrossbow/TOGGatlingCrossbowPatch.cs
@@ -1,17 +1,41 @@
-﻿using TOGModFramework;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using TOGModFramework;
 using UnityEngine;
 
 namespace TOGGatlingCrossbow
 {
     public class TOGGatlingCrossbowPatch : MonoBehaviour
     {
+        public string modName = "TOGGatlingCrossbow";
+        public bool isSemiAuto;
+        private bool isCrossbowBeingReloaded;
+
         public void Inject()
         {
             EventManager.OnCrossbowBoltFired += EventManagerOnonCrossbowBoltFired;
+
+            var json = FileManager.GetModJsonData(modName);
+            var data = JsonConvert.DeserializeObject<Settings>(json);
+            isSemiAuto = data.isSemiAuto;
         }
 
-        private void EventManagerOnonCrossbowBoltFired(CrossBowScript crossBowScript)
+        public IEnumerator ReloadCrossbow(CrossBowScript crossBowScript)
         {
+            isCrossbowBeingReloaded = true;
+            var currentHandController = crossBowScript.wp.isHeldRight
+                ? Player.local.controlInput.RightController
+                : Player.local.controlInput.LeftController;
+
+            if (isSemiAuto)
+            {
+                while (currentHandController.triggerPressed)
+                {
+                    yield return new WaitForFixedUpdate();
+                }
+            }
+
             if (!crossBowScript.isLoaded)
             {
                 crossBowScript.isLoaded = true;
@@ -21,6 +45,16 @@ namespace TOGGatlingCrossbow
                 crossBowScript.BowString.localPosition =
                     new Vector3(0.0f, crossBowScript.BowString.localPosition.y, 0.008f);
             }
+
+            isCrossbowBeingReloaded = false;
+
+            yield return null;
+        }
+
+        private void EventManagerOnonCrossbowBoltFired(CrossBowScript crossBowScript)
+        {
+            if (!isCrossbowBeingReloaded)
+                ConfigManager.local.StartCoroutine(ReloadCrossbow(crossBowScript));
         }
     }
 }
